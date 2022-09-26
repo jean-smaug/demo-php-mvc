3405f95d227fa1ac1daecda872602894aac04285
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2022-09-26 10:12:56 +0200

    make card a link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 // main.js is the single entry point.
 // It's used to resolve all CSS parts
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import "../css/main.css";
-
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
--- views/articles/index.php
+++ views/articles/index.php
@@ -3,19 +3,19 @@
 <ul class="List mt-4">
     <?php foreach ($articles as $article): ?>
         <li>
-            <div class="Card">
-                <div class="Card__Image">
-                    <img src="<?= $article["image"] ?>" alt="">
+            <a href="/article?id=<?= $article["id"] ?>">
+                <div class="Card">
+                    <div class="Card__Image">
+                        <img src="<?= $article["image"] ?>" alt="">
+                    </div>
+
+                    <h2 class="Card__Title"><?= $article["title"] ?></h2>
+
+                    <p class="Card__Content">
+                        <?= substr($article["body"], -50 ) ?>
+                    </p>
                 </div>
-
-                <h2 class="Card__Title"><?= $article["title"] ?></h2>
-
-                <p class="Card__Content">
-                    <?= substr($article["body"], -50 ) ?>
-                </p>
-
-                <a href="/article?id=<?= $article["id"] ?>">Voir l'article</a>
-            </div>
+            </a>
         </li>
     <?php endforeach; ?>
 </ul>
